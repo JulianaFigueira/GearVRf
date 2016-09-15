843bfdcdac11d0d01637596f8a67d0b6e8c5e4cb
Frederico Duarte <frederico.sd@samsung.com>
2016-09-15 11:24:13 -0400

    Modified methods for converting Colliders to CollisionShapes
    Signed-off-by: Frederico Duarte <frederico.sd@samsung.com>

--- GVRf/Framework/framework/src/main/jni/engine/physics/bullet/bullet_gvr_utils.h
+++ GVRf/Framework/framework/src/main/jni/engine/physics/bullet/bullet_gvr_utils.h
@@ -4,7 +4,9 @@
 
 #include <BulletDynamics/Dynamics/btRigidBody.h>
 #include "objects/components/sphere_collider.h"
+#include "objects/components/box_collider.h"
 #include <BulletCollision/CollisionShapes/btSphereShape.h>
+#include <BulletCollision/CollisionShapes/btBoxShape.h>
 
 #ifndef FRAMEWORK_BULLET_GVR_UTILS_H
 #define FRAMEWORK_BULLET_GVR_UTILS_H
@@ -12,6 +14,8 @@
 namespace gvr
 {
     btCollisionShape *convertCollider2CollisionShape(Collider *collider);
+    btCollisionShape *convertSphereCollider2CollisionShape(SphereCollider *collider);
+    btCollisionShape *convertBoxCollider2CollisionShape(BoxCollider *collider);
     btTransform convertTransform2btTransform(const Transform* t);
     void convertBtTransform2Transform(btTransform &bulletTransform, Transform* transform);
 }
