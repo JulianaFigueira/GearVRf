14d4b2e186f2255139688540548ed06cb82ecac2
Cristiano Bozzetto <c.bozzetto@sidiamao.com.br>
2017-06-02 11:31:56 -0400

    Initial documentation for Constraints
    Also fixed GVRPoint2PointConstraint.getPivotInA method signature.

--- GVRf/Extensions/gvrf-physics/src/main/java/org/gearvrf/physics/GVRConstraint.java
+++ GVRf/Extensions/gvrf-physics/src/main/java/org/gearvrf/physics/GVRConstraint.java
@@ -1,9 +1,32 @@
+/* Copyright 2015 Samsung Electronics Co., LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.gearvrf.physics;
 
 import org.gearvrf.GVRContext;
 
 import java.util.List;
 
+/**
+ * Base class to represent a constraint for the movement of two
+ * {@linkplain GVRRigidBody rigid bodies}.
+ * <p>
+ * After created anf fully configured a constraint must be attached to a
+ * {@linkplain org.gearvrf.GVRSceneObject scene object} containing a rigid body that will become
+ * the owner of this constraint (body A).
+ */
 abstract class GVRConstraint extends GVRPhysicsWorldObject {
 
     protected GVRConstraint(GVRContext gvrContext, long nativePointer) {
--- GVRf/Extensions/gvrf-physics/src/main/java/org/gearvrf/physics/GVRHingeConstraint.java
+++ GVRf/Extensions/gvrf-physics/src/main/java/org/gearvrf/physics/GVRHingeConstraint.java
@@ -21,21 +21,53 @@ import org.gearvrf.GVRContext;
  * Created by c.bozzetto on 30/05/2017.
  */
 
+/**
+ * Represents a hinge constraint that restricts two {@linkplain GVRRigidBody rigid bodies} to only
+ * rotate around one axis.
+ */
 public class GVRHingeConstraint extends GVRConstraint {
+
+    /**
+     * Constructs a new instance of hinge constraint.
+     *
+     * @param gvrContext the context of the app
+     * @param rigidBodyB the second rigid body (not the owner) in this constraint
+     * @param pivotInA the pivot point related to body A (the owner)
+     * @param pivotInB the pivot point related to body B
+     * @param axisInA the axis around which body A can rotate
+     * @param axisInB the axis around which body B can rotate
+     */
     public GVRHingeConstraint(GVRContext gvrContext, GVRRigidBody rigidBodyB, float pivotInA[],
                                  float pivotInB[], float axisInA[], float axisInB[]) {
         super(gvrContext, Native3DHingeConstraint.ctor(rigidBodyB.getNative(), pivotInA, pivotInB,
                 axisInA, axisInB));
     }
 
+    /**
+     * Set rotation limits (in radians) for the bodies.
+     *
+     * @param lower lower limit
+     * @param upper upper limit
+     */
     public void setLimits(float lower, float upper) {
         Native3DHingeConstraint.setLimits(getNative(), lower, upper);
     }
 
+    /**
+     * Gets the lower rotation limit for the constraint
+     *
+     * @return the angular limit in radians
+     */
     public float getLowerLimit() {
         return Native3DHingeConstraint.getLowerLimit(getNative());
     }
 
+
+    /**
+     * Gets the upper rotation limit for the constraint
+     *
+     * @return the angular limit in radians
+     */
     public float getUpperLimit() {
         return Native3DHingeConstraint.getUpperLimit(getNative());
     }
